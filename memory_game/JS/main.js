a0a56c1a4ac91c8c78ee8917f5a1d706f7fa8760
buddhablake <blakecdouglass@gmail.com>
2020-04-04 17:43:28 -0700

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,8 +1,27 @@
-console.log("up and running!")
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+let cardOne = cards[0];
+let cardTwo = cards[1];
+cardsInPlay.push(cardOne, cardTwo);
+console.log("User flipped " + cardsInPlay[0])
+console.log("User flipped " + cardsInPlay[1])
+
+let h1 = document.querySelector('h1');
+
+
+
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		h1.alert("You found a match!");
+	}
+
+	else{
+		h1.alert('Sorry, try again.')
+	}
+}
+
+else{
+	console.log("choose another card you fuck stick!")
+}
