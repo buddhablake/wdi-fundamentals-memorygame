a38e34b11dd911c7ca0a3c401e2efd8fce2847b3
buddhablake <blakecdouglass@gmail.com>
2020-04-04 19:42:39 -0700

    created functions to make game replayable

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,27 +1,32 @@
+
+
 const cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[1];
-cardsInPlay.push(cardOne, cardTwo);
-console.log("User flipped " + cardsInPlay[0])
-console.log("User flipped " + cardsInPlay[1])
-
-let h1 = document.querySelector('h1');
-
-
-
-
-if(cardsInPlay.length === 2){
+function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		h1.alert("You found a match!");
+		console.log("You found a match!");
 	}
 
 	else{
-		h1.alert('Sorry, try again.')
+		console.log('Sorry, try again.')
 	}
 }
 
-else{
-	console.log("choose another card you fuck stick!")
+function flipCard(cardId){
+
+console.log("User flipped " + cards[cardId])
+cardsInPlay.push(cards[cardId])
+
+	if(cardsInPlay.length === 2){
+		checkForMatch()
+     }
+
+
 }
+
+
+flipCard(0)
+flipCard(2)
+
+
